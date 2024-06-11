385c3261ccd9eaec62d02a18e3326a4ed1cd05d0
Hex <nebula@Nebula.local>
2024-06-11 18:59:16 +0800

    feat: add refine jobs with preset to xparse.

--- const.go
+++ const.go
@@ -55,7 +55,7 @@ const (
 
 	AttrIndex = "_attr_index"
 
-	AttrRegexp = "_attr_regexp"
+	AttrRegex = "_attr_regex"
 
 	// AttrPython run python script directly(python environment is required), and the print will be used as the attr value.
 	// i.e.:
